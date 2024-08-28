7e104246a93cf34ae09274a20b15a9e87013a5df
oumouma <garveyshah1595@gmail.com>
2024-08-28 18:04:08 +0300

    modify: example of the difference btwn mutability and shadowing

--- variables_and_mutablity/src/main.rs
+++ variables_and_mutablity/src/main.rs
@@ -1,9 +1,28 @@
-fn main() {
-    let mut x = 5;
-        println!("The value of x is: {x}");
-     x = 6;
-    println!("The value x is; {x}");
+// 
 
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    println!("Const: {THREE_HOURS_IN_SECONDS}");
+fn main() {
+    // shadowing
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    // shadowing applyed in changing a string type to int type
+    let spaces = "    ";
+    println!("Spaces in the first instant {spaces}");
+    let spaces = spaces.len();
+    println!("spaces.len(), : {spaces}");
+
+    // // mutabilty
+    // let mut spaces = "    ";
+    // println!{"first mut spaces: {spaces}"};
+    // spaces = spaces.len();
+    // println!("2nd mut spaces: {spaces}");
+    
 }
