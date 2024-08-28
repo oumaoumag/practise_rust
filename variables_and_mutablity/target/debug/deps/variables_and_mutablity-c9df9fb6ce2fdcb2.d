baccb66b401a295e052bb1ee3f2a3bf7c27d91ed
oumouma <garveyshah1595@gmail.com>
2024-08-28 17:43:13 +0300

    chore

---
+++ variables_and_mutablity/target/debug/deps/variables_and_mutablity-c9df9fb6ce2fdcb2.d
@@ -0,0 +1,5 @@
+/home/oumouma/practise/practise_rust/variables_and_mutablity/target/debug/deps/variables_and_mutablity-c9df9fb6ce2fdcb2: src/main.rs
+
+/home/oumouma/practise/practise_rust/variables_and_mutablity/target/debug/deps/variables_and_mutablity-c9df9fb6ce2fdcb2.d: src/main.rs
+
+src/main.rs:
---
+++ variables_and_mutablity/target/debug/variables_and_mutablity.d
@@ -0,0 +1,1 @@
+/home/oumouma/practise/practise_rust/variables_and_mutablity/target/debug/variables_and_mutablity: /home/oumouma/practise/practise_rust/variables_and_mutablity/src/main.rs
