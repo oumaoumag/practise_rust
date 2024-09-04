54675159cf39a6982bb1510b915929b55a37bafc
oumouma <garveyshah1595@gmail.com>
2024-09-04 21:47:51 +0300

    vectors

---
+++ vectors1/target/debug/deps/vectors1-dd4d613678ee98f6.d
@@ -0,0 +1,5 @@
+/home/oumouma/practise/practise_rust/vectors1/target/debug/deps/vectors1-dd4d613678ee98f6: src/main.rs
+
+/home/oumouma/practise/practise_rust/vectors1/target/debug/deps/vectors1-dd4d613678ee98f6.d: src/main.rs
+
+src/main.rs:
---
+++ vectors1/target/debug/vectors1.d
@@ -0,0 +1,1 @@
+/home/oumouma/practise/practise_rust/vectors1/target/debug/vectors1: /home/oumouma/practise/practise_rust/vectors1/src/main.rs
