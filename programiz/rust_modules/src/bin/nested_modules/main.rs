95e7136977f210dbb7f86a88ae6ed1f33e9cc158
oumouma <garveyshah1595@gmail.com>
2024-09-07 00:55:35 +0300

    Using the  keyword in rust

--- programiz/rust_modules/src/bin/nested_modules/main.rs
+++ programiz/rust_modules/src/bin/nested_modules/main.rs
@@ -4,6 +4,14 @@
 /* Note: 
         The `::` operator is used to separate the module name and the item to call inside the module*/ 
 
+/* The Use KeyWord in Rust */
+
+/* we can use the `use` keyword to bring items inside a module into the current scope.
+ It helps us eliminate writing out the full module path to call functions. */
+
+// bring the create function into scope
+use player::sprite::create;
+
 pub mod player {
     pub mod sprite {
         pub fn create() {
@@ -14,5 +22,5 @@ pub mod player {
 
 fn main() {
     // call public function create from sprite module which is inside player module
-    player::sprite::create();
+    create();
 }
