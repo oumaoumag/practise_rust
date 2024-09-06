b80a72639c8e74cf6b8d88e83aa55cbc8e9b9283
oumouma <garveyshah1595@gmail.com>
2024-09-06 23:35:50 +0300

    VIsibility of items inside a Module in Rust

--- programiz/rust_modules/src/main.rs
+++ programiz/rust_modules/src/main.rs
@@ -5,19 +5,39 @@ git fn main() {
     /*& Defining a Module in Rust */
 // The `mod` keyword is used to define a modul. 
     // Syntax of a module
-    mod module_name {
-        // code
-    }
+    // mod module_name {
+    //     // code
+    // }
 
    
 }
 
  //  a module named config
+//  mod config {
+//     // a function print inside of the module
+//     fn print() {
+//         println!("config!");
+//     }
+//  }
+
+ /* Visibility of Items inside a Module in Rust */
+
+ // Items inside a module can be private or public. By default, a module is private.
+ // The `pub` keyword can be used to give a an item  public visibility.
+ 
  mod config {
-    // a function print inside of the module
-    fn print() {
-        println!("config!");
-    }
+    mod config {
+        // items in modules by default have private visibility
+        fn select() {
+            println!("called config::select");
+        }
+    
+        // use the `pub` keyword to override private visibility
+        pub fn print() {
+            println!("called config::print");
+        }
+    } 
  }
 
- 
+
+
