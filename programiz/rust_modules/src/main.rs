c658ff16f92ae1c9794c39a1e626e0703a3c8621
oumouma <garveyshah1595@gmail.com>
2024-09-07 00:29:30 +0300

    USing Module in Rust

--- programiz/rust_modules/src/main.rs
+++ programiz/rust_modules/src/main.rs
@@ -1,43 +1,26 @@
-fn main() {
-    // Modules in Rust help  in splitting a program into logical units for better readability and organization.
-    // A module is a collection of items: functions, structs and even other modules
+/* Using Module in Rust*/
 
-    /*& Defining a Module in Rust */
-// The `mod` keyword is used to define a modul. 
-    // Syntax of a module
-    // mod module_name {
-    //     // code
-    // }
+mod player {
+    // private fuction
+    fn focus() {
+        println!("Called plater::focus");
+    }
 
-    // public items inside module can be accessed outside the parent module.
-    // call public print function from display module
-    // the operator `::` is used to separate the module name and the item to call inside the module.
-    config:: print();
-       select();
-   
+    // Public function
+    pub fn shift() {
+        println!("called player::shift");
+    }
+
+    // public function
+    pub fn jump() {
+        // call private function focus and shift inside the module
+        focus();
+        shift();
+        println!("called player::jump");
+    }
 }
 
- //  a module named config
-//  mod config {
-//     // a function print inside of the module
-//     fn print() {
-//         println!("config!");
-//     }
-//  }
-
- /* Visibility of Items inside a Module in Rust */
-
- // Items inside a module can be private or public. By default, a module is private.
- // The `pub` keyword can be used to give a an item  public visibility.
- 
- mod config {
-    // items in modules by default have private visibility
-    fn select() {
-        println!("called config::select");
-       }
-
-       // use the  `pub` keyword to override private visibility
-       pub fn print() {
-        println!("called config::print");
-       }
+fn main() {
+    // call public function jump from player module
+    player::jump();
 }
