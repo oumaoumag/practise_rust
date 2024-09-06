a1abf238578018dc9c7158a85812a1302d8cf868
oumouma <garveyshah1595@gmail.com>
2024-09-07 02:38:52 +0300

    changing the value of a variable

--- programiz/variables_and_mutability/src/main.rs
+++ programiz/variables_and_mutability/src/main.rs
@@ -16,8 +16,19 @@ fn main() {
 
     // Variable to store a string
     let name = "Jackie";
-    println!("Name: {}", name)
+    println!("Name: {}", name);
 
     /* Change Value of a Variable */
+/* By default, Rust variables are immutabl, meaning we cannot change the value of a variable once it is defined. */
+
+// declare a variable with value 1
+    let x = 1;
+    println!("x = {}", x);
+
+    // change the value of variable x
+    x = 2;
+    println!("x = {}", x);
+    /* running the above code give an error because we are trying to change the value of the `x` variable
+        from  `1` to `2` */
 
 }
---
+++ programiz/variables_and_mutability/target/debug/deps/variables_and_mutability-7c025e0231f4248a.d
@@ -0,0 +1,5 @@
+/home/oumouma/practise/practise_rust/programiz/variables_and_mutability/target/debug/deps/libvariables_and_mutability-7c025e0231f4248a.rmeta: src/main.rs
+
+/home/oumouma/practise/practise_rust/programiz/variables_and_mutability/target/debug/deps/variables_and_mutability-7c025e0231f4248a.d: src/main.rs
+
+src/main.rs:
