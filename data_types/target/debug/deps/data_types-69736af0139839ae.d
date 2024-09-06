3caa28ea7f2e0401ff788232d964b749de166d93
oumouma <garveyshah1595@gmail.com>
2024-09-07 01:02:39 +0300

    chore

---
+++ data_types/target/debug/deps/data_types-69736af0139839ae.d
@@ -0,0 +1,5 @@
+/home/oumouma/practise/practise_rust/data_types/target/debug/deps/libdata_types-69736af0139839ae.rmeta: src/main.rs
+
+/home/oumouma/practise/practise_rust/data_types/target/debug/deps/data_types-69736af0139839ae.d: src/main.rs
+
+src/main.rs:
---
+++ data_types/target/debug/deps/data_types-872a0ca6ffceecec.d
@@ -0,0 +1,5 @@
+/home/oumouma/practise/practise_rust/data_types/target/debug/deps/libdata_types-872a0ca6ffceecec.rmeta: src/main.rs
+
+/home/oumouma/practise/practise_rust/data_types/target/debug/deps/data_types-872a0ca6ffceecec.d: src/main.rs
+
+src/main.rs:
