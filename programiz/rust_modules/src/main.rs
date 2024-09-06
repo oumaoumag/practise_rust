5c5bda77bd16453fb915a9649a5c943b8233e8b8
oumouma <garveyshah1595@gmail.com>
2024-09-06 23:30:35 +0300

    defining a module in rust

--- programiz/rust_modules/src/main.rs
+++ programiz/rust_modules/src/main.rs
@@ -1,3 +1,23 @@
 git fn main() {
-    println!("Hello, world!");
+    // MOdules in Rust help  in splitting a program into logical units for better readability and organization.
+    // A module is a collection of items: functions, structs and even other modules
+
+    /*& Defining a Module in Rust */
+// The `mod` keyword is used to define a modul. 
+    // Syntax of a module
+    mod module_name {
+        // code
+    }
+
+   
 }
+
+ //  a module named config
+ mod config {
+    // a function print inside of the module
+    fn print() {
+        println!("config!");
+    }
+ }
+
+ 
