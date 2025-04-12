48924c687481b1fba51d564bb38c5ae0f874250b
garveyshah <garveyshah1595@gmail.com>
2025-04-12 05:09:31 +0300

    Add loop labels example and update main to include new function

--- the_rust_book/common_programming_concepts/control_flow/repetition_with_loops/src/lib.rs
+++ the_rust_book/common_programming_concepts/control_flow/repetition_with_loops/src/lib.rs
@@ -17,3 +17,40 @@ pub fn returning_values_from_loops() {
 
     println!("The result  is {result}");
 }
+
+// If you have loops within loops, 'break' and 'continue' apply to the innermost loop at that point.
+// You can optionally spcify a 'loop label' on a loop that can then use with 'break' or 'continue' to specify that
+// you can use wth 'break' or 'continue' to spcify that those keywords apply to the labled loop instead of the innermost loop.
+// Loop labels must begin with a signle quote.
+pub fn loop_labels_to_disambiguate_btn_multple_loops() {
+    let mut count = 0;
+
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+pub fn conditional_loops_with_while {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+    println!()
+}
--- the_rust_book/common_programming_concepts/control_flow/repetition_with_loops/src/main.rs
+++ the_rust_book/common_programming_concepts/control_flow/repetition_with_loops/src/main.rs
@@ -1,7 +1,9 @@
 // use repetition_with_loops::repeating_code_with_loop;
 use repetition_with_loops::returning_values_from_loops;
+use repetition_with_loops::loop_labels_to_disambiguate_btn_multple_loops;
 
 fn main() {
-    // repeating_code_with_loop()
-    returning_values_from_loops()
+    // repeating_code_with_loop();
+    returning_values_from_loops();
+    loop_labels_to_disambiguate_btn_multple_loops();
 }
