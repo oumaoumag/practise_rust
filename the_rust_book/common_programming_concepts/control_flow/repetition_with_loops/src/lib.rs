55f22f04519134ee7da29dc2d0a5183e9fb85102
garveyshah <garveyshah1595@gmail.com>
2025-04-12 05:01:21 +0300

    Refactor loop functions and update main to demonstrate returning values from loops

--- the_rust_book/common_programming_concepts/control_flow/repetition_with_loops/src/lib.rs
+++ the_rust_book/common_programming_concepts/control_flow/repetition_with_loops/src/lib.rs
@@ -3,3 +3,17 @@ pub fn repeating_code_with_loop() {
         println!("again!");
     }
 }
+
+pub fn returning_values_from_loops() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result  is {result}");
+}
--- the_rust_book/common_programming_concepts/control_flow/repetition_with_loops/src/main.rs
+++ the_rust_book/common_programming_concepts/control_flow/repetition_with_loops/src/main.rs
@@ -1,5 +1,7 @@
-use repetition_with_loops::repeating_code_with_loop;
+// use repetition_with_loops::repeating_code_with_loop;
+use repetition_with_loops::returning_values_from_loops;
 
 fn main() {
-    repeating_code_with_loop()
+    // repeating_code_with_loop()
+    returning_values_from_loops()
 }
