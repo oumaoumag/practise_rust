3044686c16e7e8f0d969cd1bbc103df7f464acff
garveyshah <garveyshah1595@gmail.com>
2025-04-17 03:45:46 +0300

    Refactor next_prime function for clarity and consistency

--- checkpoint/level_1/nextprime/src/lib.rs
+++ checkpoint/level_1/nextprime/src/lib.rs
@@ -1,27 +1,29 @@
 pub fn next_prime(nbr: u64) -> u64 {
-    let mut candidate = nbr;
-    while !is_prime(candidate) {
-        candidate += 1;
+    let mut prime = nbr;
+
+    while !is_prime(prime) {
+        prime += 1;
     }
-    candidate
+    prime
 }
 
-pub fn is_prime(n : u64) -> bool {
+fn  is_prime(n: u64) -> bool {
     if n <= 1 { return false; }
     if n <= 3 { return true; }
-
-    if n % 2 == 0|| n % 3 == 0 { return false; }
+    
+    if n % 2 == 0 || n % 3 == 0 {
+         return false;
+     }
 
     let sqrt = (n as f64).sqrt() as u64;
-    let mut i  = 5;
+    let mut i = 5;
 
     while i < sqrt {
-        if n % i == 0 || n % (i + 2 )  == 0 {
+        if n % i == 0 || n % (i + 2) == 0 {
             return false;
         }
-        i += 6
+        i += 6;
     }
-
     true
 }
 
