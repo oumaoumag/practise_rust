6cd2bf29f83f43cc10e5b7965afec7354768a255
oumaoumag <ouma.godwin10@gmail.com>
2025-04-27 22:55:10 +0300

    Add initial implementation of count_factorial_steps function with README and main entry point

---
+++ checkpoint/exam_4/level_1/count_factorial_steps/src/main.rs
@@ -0,0 +1,7 @@
+use count_factorial_steps::*;
+
+fn main() {
+    println!("The factorial steps of 720 is {:?}", count_factorial_steps(720));
+    println!("The factorial steps of 63 is: {:?}", count_factorial_steps(63));
+    println!("The factorial steps of 6 {:?}", count_factorial_steps(6));
+}
