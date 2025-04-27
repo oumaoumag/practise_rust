5cfa4c1d018a3510d94064d15b86af0e52491d16
oumaoumag <ouma.godwin10@gmail.com>
2025-04-27 21:32:13 +0300

    Implement reverse_it function with detailed README instructions and usage examples

--- checkpoint/exam_3/level_1/reverse_it/src/lib.rs
+++ checkpoint/exam_3/level_1/reverse_it/src/lib.rs
@@ -1,4 +1,14 @@
-pub fn reverse_it(v: i32) -> String {
-    format!("{v:?}").chars().rev().collect()
-}
+pub fn reverse_it(i_nbr: i32) -> String {
+    let original = i_nbr.to_string();
+    let reversed: String = original
+    .chars()
+    .filter(|&c| c != '-') // to ignore '-' if its negative
+    .rev()
+    .collect();
 
+    if i_nbr < 0 {
+        format!("-{}{}", reversed, original.trim_start_matches('-'))
+    } else {
+        format!("{}{}", reversed, original)
+    }
+}
